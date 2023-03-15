b4df4478c420ecd14e112cd1436866b491c8e77f
adbenitez <asieldbenitez@gmail.com>
2023-03-15 17:35:44 -0400

    add model subcommand

--- voice2text_deltabot/subcommands.py
+++ voice2text_deltabot/subcommands.py
@@ -10,10 +10,11 @@ from .const import MODEL_CFG_KEY
 def add_subcommands(cli: BotCli) -> None:
     cli.add_subcommand(model).add_argument(
         "model",
-        help="set the whisper model to use, see https://github.com/openai/whisper/blob/main/model-card.md for possible values",
+        help="the whisper model, for possible values see https://github.com/openai/whisper/blob/main/model-card.md",
     )
 
 
 async def model(bot: Bot, args: Namespace) -> None:
+    """set the whisper model to use"""
     await bot.account.set_config(MODEL_CFG_KEY, args.model)
     logging.info("model=%s", args.model)
