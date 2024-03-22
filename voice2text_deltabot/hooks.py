18957f8a5651ecf75f98fddbdb1b371045668bcf
adbenitez <asieldbenitez@gmail.com>
2024-03-22 08:24:36 -0400

    improve help message, remove messages after usage

--- voice2text_deltabot/hooks.py
+++ voice2text_deltabot/hooks.py
@@ -25,7 +25,11 @@ cli.add_generic_option(
     choices=["int8", "float16", "int8_float16"],
     default="int8",
 )
-STATUS = "I'm a Delta Chat bot, send me any voice message to convert it to text"
+HELP = (
+    "I'm a Delta Chat bot, send me any voice message to convert it to text,"
+    " you can also use me in groups.\n\nNo 3rd party service is involved,"
+    " only I will have access for a short period of time to the voice messages you send to me."
+)
 MODEL: WhisperModel = None  # noqa
 
 
@@ -39,7 +43,10 @@ def _on_init(bot: Bot, args: Namespace) -> None:
     for accid in bot.rpc.get_all_account_ids():
         if not bot.rpc.get_config(accid, "displayname"):
             bot.rpc.set_config(accid, "displayname", "Voice To Text")
-            bot.rpc.set_config(accid, "selfstatus", STATUS)
+            status = (
+                "I'm a Delta Chat bot, send me any voice message to convert it to text"
+            )
+            bot.rpc.set_config(accid, "selfstatus", status)
             bot.rpc.set_config(accid, "delete_server_after", "1")
             bot.rpc.set_config(accid, "delete_device_after", str(60 * 60 * 24))
 
@@ -62,7 +69,7 @@ def _log_event(bot: Bot, accid: int, event: AttrDict) -> None:
         if event.progress == 1000:
             bot.logger.debug("QR scanned by contact id=%s", event.contact_id)
             chatid = bot.rpc.create_chat_by_contact_id(accid, event.contact_id)
-            bot.rpc.send_msg(accid, chatid, {"text": STATUS})
+            bot.rpc.send_msg(accid, chatid, {"text": HELP})
 
 
 @cli.on(events.NewMessage(is_info=False, is_bot=None))
@@ -72,6 +79,7 @@ def on_newmsg(bot: Bot, accid: int, event: AttrDict) -> None:
     if chat.chat_type == ChatType.SINGLE:
         bot.rpc.markseen_msgs(accid, [msg.id])
         if msg.is_bot:
+            bot.rpc.delete_messages([msg.id])
             return
 
     if msg.view_type in (ViewType.VOICE, ViewType.AUDIO):
@@ -86,4 +94,6 @@ def on_newmsg(bot: Bot, accid: int, event: AttrDict) -> None:
         )
         bot.rpc.send_msg(accid, msg.chat_id, {"text": text, "quotedMessageId": msg.id})
     elif chat.chat_type == ChatType.SINGLE:
-        bot.rpc.send_msg(accid, msg.chat_id, {"text": STATUS})
+        bot.rpc.send_msg(accid, msg.chat_id, {"text": HELP})
+
+    bot.rpc.delete_messages([msg.id])
